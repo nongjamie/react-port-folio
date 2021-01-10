d91bbe4531881aa66b8c2bde010ecd7bf75b7c0b
Sathira Kittisukmongkol <nong.jamie.jamie@gmail.com>
2021-01-11 00:13:10 +0700

    Edit the text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This web app will be Jamie's port folio
         </p>
         <a
           className="App-link"
